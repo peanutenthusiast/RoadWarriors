650a23258bde07f52563a5567c9185836d3290e1
peanutenthusiast <mhdedios@gmail.com>
2017-11-30 12:40:12 -0800

    Correct interpolation for params in index method

--- app/controllers/api/gas_stations_controller.rb
+++ app/controllers/api/gas_stations_controller.rb
@@ -2,9 +2,7 @@ module Api
 
   class GasStationsController < ApplicationController
     def index
-      response = HTTParty.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude},${longitude}&radius=500&type=gas_station&key=${ Figaro.env.google_places}" )
-
-https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&type=restaurant&keyword=cruise&key=
+      response = HTTParty.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=#{params[:latitude]},#{params[:longitude]}&radius=500&type=gas_station&key=${ Figaro.env.google_places}" )
 
       render json: response
     end

ddedc145a29aa3bdd7e1eae5b98ca3b8b299724a
peanutenthusiast <mhdedios@gmail.com>
2018-01-11 18:43:45 -0800

    Modify gas stations controller to return results

--- app/controllers/api/gas_stations_controller.rb
+++ app/controllers/api/gas_stations_controller.rb
@@ -2,13 +2,11 @@ module Api
 
   class GasStationsController < ApplicationController
     def index 
-      response = HTTParty.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=#{params[:latitude]},#{params[:longitude]}&radius=500&type=gas_station&key=#{ Figaro.env.google_places}" )
-
-      results = response["results"]
+      response = HTTParty.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=#{params[:latitude]},#{params[:longitude]}&radius=8500&type=gas_station&key=#{Figaro.env.google_places}" )
+      results = JSON.parse(response.body)["results"]
+      p results
       render json: results
     end
   end
 
 end
-
-#         axios({ method: 'get', url: `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude},${longitude}&radius=8500&type=gas_station&key=${Config.GooglePlaces}` }).catch(response => { console.log(response); })

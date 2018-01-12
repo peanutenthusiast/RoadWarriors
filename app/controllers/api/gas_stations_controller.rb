5577d4199a0444b7aba426f66351687db22b2821
peanutenthusiast <mhdedios@gmail.com>
2018-01-11 18:49:26 -0800

    Modify gas stations controller to return 10 results

--- app/controllers/api/gas_stations_controller.rb
+++ app/controllers/api/gas_stations_controller.rb
@@ -3,9 +3,8 @@ module Api
   class GasStationsController < ApplicationController
     def index 
       response = HTTParty.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=#{params[:latitude]},#{params[:longitude]}&radius=8500&type=gas_station&key=#{Figaro.env.google_places}" )
-      results = JSON.parse(response.body)["results"]
-      p results
-      render json: results
+      sliced_stations = JSON.parse(response.body)["results"].slice(0, 10)
+      render json: sliced_stations
     end
   end
 

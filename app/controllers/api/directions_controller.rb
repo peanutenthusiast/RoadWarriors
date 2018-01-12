604c4a2eb374782d223b39f64be43d5927d5bc25
peanutenthusiast <mhdedios@gmail.com>
2018-01-11 18:01:10 -0800

    Correct interpolation for direcions key

--- app/controllers/api/directions_controller.rb
+++ app/controllers/api/directions_controller.rb
@@ -2,7 +2,7 @@ module Api
 
   class DirectionsController < ApplicationController
     def index
-      response = HTTParty.get("https://maps.googleapis.com/maps/api/directions/json?origin=#{latitude},#{longitude}&destination=#{destination}&key=${ Figaro.env.google_directions}")
+      response = HTTParty.get("https://maps.googleapis.com/maps/api/directions/json?origin=#{params[:latitude]},#{params[:longitude]}&destination=#{params[:destination]}&key=#{ Figaro.env.google_directions}")
       points = JSON.parse(response.body)["routes"][0]["overview_polyline"]["points"]
       render json: points
     end
